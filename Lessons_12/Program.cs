affc9012780da6dbeca4f29617ad2025762f8d0e
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-11-14 22:38:13 +0300

    Добавьте файлы проекта.

---
+++ Lessons_12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Lessons_12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            String login;
+            String pass;
+            String conpass;
+
+
+           
+            Console.WriteLine("Start in singup!");
+
+            Console.WriteLine("(in word) Login:");
+            login = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Password:");
+            pass = Convert.ToString(Console.ReadLine());
+            
+            Console.WriteLine("ConfirmPassword");
+            conpass = Convert.ToString(Console.ReadLine());
+            
+            Console.WriteLine("Acount Created Name: " + login);
+ 
+             Console.WriteLine("Login form!");
+     
+         
+                Console.WriteLine("Password Corect");
+
+            Console.WriteLine("You successfully LOGIN!!!");
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
